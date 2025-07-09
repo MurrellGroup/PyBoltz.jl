bd4932080bc8d8c14cba1ffdf54f6990794e62bf
aronStalmarck <aron.stalmarck@gmail.com>
2025-07-09 14:05:09 +0200

    remove PythonCall

--- src/PyBoltz.jl
+++ src/PyBoltz.jl
@@ -1,7 +1,5 @@
 module PyBoltz
 
-import PythonCall # for boltz predict command
-
 using Scratch: @get_scratch!, delete_scratch!
 const CACHE_KEY = "boltz_cache"
 clear_cache() = delete_scratch!(CACHE_KEY)
